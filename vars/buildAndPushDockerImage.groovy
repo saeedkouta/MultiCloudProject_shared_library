11aafe1d9369e3fa8b87f3167a539ce34ef0d929
saeedkouta7 <saeedkouta7@gmail.com>
2024-06-12 19:31:51 +0300

    Update buildAndPushDockerImage.groovy

--- vars/buildAndPushDockerImage.groovy
+++ vars/buildAndPushDockerImage.groovy
@@ -1,12 +1,15 @@
 #!usr/bin/env groovy
-def call(String dockerHubCredentialsID, String imageName) {
+def call(String dockerHubCredentialsID, imageName) {
 
-	// Log in to DockerHub 
-	withCredentials([usernamePassword(credentialsId: "${dockerHubCredentialsID}", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
-		sh "docker login -u ${USERNAME} -p ${PASSWORD}"
+        // Build Docker image
+        echo "Building Docker image..."
+        sh "docker build -t ${imageName}:v1 ."
+  
+  	    // Log in to DockerHub 
+	      withCredentials([usernamePassword(credentialsId: "${dockerHubCredentialsID}", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+		    sh "docker login -u ${USERNAME} -p ${PASSWORD}"
         }
-        
-        // Build and push Docker image
+        // Push Docker image
         echo "Pushing Docker image..."
-        sh "docker push ${imageName}:v1"	 
+        sh "docker push ${imageName}:v1"	
 }
