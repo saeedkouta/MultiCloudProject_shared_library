2ebf491acac82e24f0795379b0e3d50436726196
saeedkouta7 <saeedkouta7@gmail.com>
2024-06-12 20:57:36 +0300

    Update buildAndPushDockerImage.groovy

--- vars/buildAndPushDockerImage.groovy
+++ vars/buildAndPushDockerImage.groovy
@@ -1,15 +1,24 @@
 #!usr/bin/env groovy
 def call(String dockerHubCredentialsID, imageName, BUILD_NUMBER) {
 
-        // Build Docker image
-        echo "Building Docker image..."
-        sh "docker build -t ${imageName}:${BUILD_NUMBER} ."
-  
-  	    // Log in to DockerHub 
-	      withCredentials([usernamePassword(credentialsId: "${dockerHubCredentialsID}", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
-		    sh "docker login -u ${USERNAME} -p ${PASSWORD}"
-        }
-        // Push Docker image
-        echo "Pushing Docker image..."
-        sh "docker push ${imageName}:${BUILD_NUMBER}"	
+    // Build Docker image
+    echo "Building Docker image..."
+    sh "docker build -t ${imageName}:${BUILD_NUMBER} ."
+
+    // Tag Docker image with latest
+    sh "docker tag ${imageName}:${BUILD_NUMBER} ${imageName}:latest"
+
+    // Log in to DockerHub 
+    withCredentials([usernamePassword(credentialsId: "${dockerHubCredentialsID}", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+        sh "docker login -u ${USERNAME} -p ${PASSWORD}"
+    }
+
+    // Push Docker image with build number
+    echo "Pushing Docker image with build number..."
+    sh "docker push ${imageName}:${BUILD_NUMBER}"
+    
+    // Push Docker image with latest tag
+    echo "Pushing Docker image with latest tag..."
+    sh "docker push ${imageName}:latest"
 }
+
