1e262a4242e2c46784baed1d3592f79f41fda66d
saeedkouta7 <saeedkouta7@gmail.com>
2024-06-13 19:36:15 +0300

    Update sonarQubeAnalysis.groovy

--- vars/sonarQubeAnalysis.groovy
+++ vars/sonarQubeAnalysis.groovy
@@ -1,10 +1,22 @@
-#!usr/bin/env groovy
-def call(){ 
-	echo "Running SonarQube "
-	withSonarQubeEnv(credentialsId: 'sonar-token') {
-		echo "Running SonarQube Analysis..."
-		sh ''' $SCANNER_HOME/bin/sonar-scanner -Dsonar.projectName=saeed \
-		-Dsonar.java.binaries=. \
-		-Dsonar.projectKey=saeed '''
-	}
+#!/usr/bin/env groovy
+
+def call() {
+    echo "Running SonarQube Analysis..."
+
+    // Define your SonarQube server configuration credentials ID
+    def sonarqubeCredentialsId = 'sonar-token'
+
+    // Define other properties for SonarQube analysis
+    def projectName = 'saeed'
+    def projectKey = 'saeed'
+
+    // Run withSonarQubeEnv to inject SonarQube environment variables
+    withSonarQubeEnv(credentialsId: sonarqubeCredentialsId) {
+        sh """
+        ${tool 'SonarQube'}  // Optionally specify SonarQube scanner tool installation if configured in Jenkins
+        sonar-scanner -Dsonar.projectName=${projectName} \
+                      -Dsonar.projectKey=${projectKey} \
+                      -Dsonar.java.binaries=.
+        """
+    }
 }
